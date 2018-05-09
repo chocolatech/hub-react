c0d950f0af4f9208b2495505520cf9d3fcb79b54
cobbie <evelinagonda@gmail.com>
2018-05-09 20:56:09 +0200

    add debounce

--- src/search-bar/search.component.js
+++ src/search-bar/search.component.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import debounce from 'lodash/debounce';
+
+// var debounce = require('debounce');
 
 export class Search extends Component {
 
+    componentDidMount(){
+        // this.onChange = debounce(this.props.onChange, 2000);
+        // this.onKeyPress = debounce(this.props.onKeyPress, 7000);
+
+        this.onChange = debounce(this.props.onChange, 500);
+    }
+
+    dupa = (e) => {
+        e.persist();
+        this.onChange(e.target.value);
+    }
+
     render() {
+        console.log('.');
         return (
             <div>
-                <input value = {this.props.username} onChange={(event) => {this.props.onChange(event.target.value)}}
+                <input value = {this.props.username} onChange={this.dupa}
                 onKeyPress={this.props.onKeyPress}/>
                 <button onClick={this.props.onSearch}>Search</button>                
             </div>
