2f62fd9c4a079cd6f9a98178ac4a88ccbe3a03f1
cobbie <evelinagonda@gmail.com>
2018-05-22 20:58:45 +0200

    bootstrap redux router

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Search } from './search-bar/search.component';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import store from './store';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { ConnectedRouter } from 'react-router-redux';
+import { store, history } from './store';
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router>
-            <Route path="/" component={App} />
-        </Router>
+        <ConnectedRouter history={history}>
+            <Switch>
+                <Route path="/" exact component={App} />
+                <Route path="/lol" component={Search} />
+            </Switch>
+        </ConnectedRouter>
 
     </Provider>, document.getElementById('root'));
 registerServiceWorker();
--- src/store.js
+++ src/store.js
@@ -4,18 +4,20 @@ import createSagaMiddleware from 'redux-saga';
 import createGlobalReducer from './global-reducer';
 import globalSagas from './global-sagas';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createHistory from 'history/createBrowserHistory';
+import { routerMiddleware } from 'react-router-redux';
 
 const sagaMiddleware = createSagaMiddleware();
+export const history = createHistory(); 
 
 const middlewares = [
   sagaMiddleware,
+  routerMiddleware(history),
 ];
 
-const store = createStore(
+export const store = createStore(
   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   createGlobalReducer(),composeWithDevTools(applyMiddleware(...middlewares))
 );
 
 sagaMiddleware.run(globalSagas);
-
-export default store;
